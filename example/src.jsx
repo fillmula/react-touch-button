bf00074fff07f1b47dad30f78730fea90dbb4d61
Zhang Kai Yu <yeannylam@gmail.com>
2016-06-01 19:49:02 +0800

    Deprecated

--- example/src.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import TextButton from '../src';
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(<TextButton touchUpInside={function(){alert("RIGHT!")}}>Button</TextButton>, document.querySelector('#button'));
-});
